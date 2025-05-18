24ef8303c31008bdb5d2894e918a8c9a5ec74730
Lucas Oliva <lucas.l.oliva@gmail.com>
2025-05-17 21:13:39 -0400

    Track .keras files with Git LFS

--- Retrain.py
+++ Retrain.py
@@ -1,8 +1,8 @@
 import os
 from glob import glob
 from PIL import Image
-import tensorflow as tf
-import numpy as np
+import tensorflow as tf # type: ignore
+import numpy as np # type: ignore
 finalImagesAns = []
 finalImagesX = []
 targetsize = (224, 224)
@@ -35,56 +35,38 @@ testdataX1.append(img_array.flatten())
 testdataX1 = np.array(testdataX1)  # Convert list of arrays to 2D numpy array
 
 testdataX2 = []
-img = Image.open("test/IMG_6812.webp").convert("RGB")
+img = Image.open("test/IMG_6798.webp").convert("RGB")
 img = img.resize(targetsize)
 img_array = np.array(img) / 255.0
 testdataX2.append(img_array.flatten())
 testdataX2 = np.array(testdataX2)  # Convert list of arrays to 2D numpy array
 
 testdataX3 = []
-img = Image.open("test/IMG_6913.webp").convert("RGB")
+img = Image.open("test/IMG_6812.webp").convert("RGB")
 img = img.resize(targetsize)
 img_array = np.array(img) / 255.0
 testdataX3.append(img_array.flatten())
 testdataX3 = np.array(testdataX3)
 
 testdataX4 = []
-img = Image.open("test/IMG_6968 (2).webp").convert("RGB")
+img = Image.open("test/IMG_6813.webp").convert("RGB")
 img = img.resize(targetsize)
 img_array = np.array(img) / 255.0
 testdataX4.append(img_array.flatten())
 testdataX4 = np.array(testdataX4)
 
 testdataX5 = []
-img = Image.open("test/PXL_20250517_222705654.webp").convert("RGB")
+img = Image.open("test/IMG_6917.webp").convert("RGB")
 img = img.resize(targetsize)
 img_array = np.array(img) / 255.0
 testdataX5.append(img_array.flatten())
 testdataX5 = np.array(testdataX5)
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Reshape((224, 224, 3), input_shape=(150528,)),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Conv2D(256, (3, 3), activation='relu'),
-    tf.keras.layers.Conv2D(256, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Conv2D(512, (3, 3), activation='relu'),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(200, activation='relu'),
-    tf.keras.layers.Dense(136, activation='relu'),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(4, activation='softmax')
-])
-model.compile(loss="categorical_crossentropy",optimizer="adam")
-model.fit(finalImagesX , finalImagesAns, epochs=45)
-eval = model.evaluate(finalImagesX,finalImagesAns)
+model = tf.keras.models.load_model('my_model.keras')
+model.fit(finalImagesX , finalImagesAns, epochs=75)
 print(model.predict(testdataX1)*100)
 print(model.predict(testdataX2)*100)
 print(model.predict(testdataX3)*100)
 print(model.predict(testdataX4)*100)
 print(model.predict(testdataX5)*100)
-model.save('my_model7.keras')
+model.save('my_model3.keras')
